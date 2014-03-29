2e7e2efbc8d74ceac5853202d83513ed614f3e4b
Quentin Geissmann <qgeissmann@gmail.com>
2014-03-29 18:27:52 +0000

    minor changes

--- src/sleepysnail/acquisition/video_capture.py
+++ src/sleepysnail/acquisition/video_capture.py
@@ -9,17 +9,18 @@ from sleepysnail.utils.logger import Logger
 #import matplotlib.animation as animation
 
 
-VIDEO_CHUNK_SIZE = 100
-MAX_CAM_NUMBER = 1
-#VIDEO_FORMAT = cv.CV_FOURCC('D', 'I', 'V', 'X')
-#VIDEO_FORMAT = cv.CV_FOURCC('M','J','P','G')
-VIDEO_FORMAT = cv.CV_FOURCC('H','2','6','4')
+VIDEO_CHUNK_SIZE = 1000
+MAX_CAM_NUMBER = 10
+#
+VIDEO_FORMAT = {'fourcc':cv.CV_FOURCC('D', 'I', 'V', 'X'), 'extension':"avi"}
+#VIDEO_FORMAT = {'fourcc':cv.CV_FOURCC('D', 'I', 'V', '3'), 'extension':"avi"}
 
+#~ VIDEO_FORMAT = {'fourcc':cv.CV_FOURCC('D', 'I', 'V', 'X'), 'extension':"avi"}
+#~ VIDEO_FORMAT = {'fourcc':cv.CV_FOURCC('M', 'P', 'E', 'G'), 'extension':"mpeg"}
 
-FPS = 5
 
 class AutoCaptureCollection(list):
-    def __init__(self, out_dir):
+    def __init__(self, out_dir, fps):
         super(AutoCaptureCollection, self).__init__()
 
 
@@ -28,21 +29,21 @@ class AutoCaptureCollection(list):
 
             try:
                 Logger.info("Probing camera #{0}".format(i))
-                cap = AutoVideoCapture(i, out_dir)
+                cap = AutoVideoCapture(i, out_dir, fps)
                 self.append(cap)
 
             except Exception as e:
-                print e
+                Logger.warning(e)
                 pass
 
         Logger.info("Managed to open {0} cameras".format(len(self)))
-        print self
+
 
 
 
 
 class AutoVideoCapture(object):
-    def __init__(self, idx, out_dir):
+    def __init__(self, idx, out_dir, fps):
 
         #super(AutoVideoCapture, self).__init__(device=idx)
         self.stream = cv2.VideoCapture(device=idx)
@@ -51,8 +52,6 @@ class AutoVideoCapture(object):
             raise Exception("cannot open this camera")
             #try to get the first frame
             ret,frame = self.stream.read()
-            print  "-------------"
-            print frame
             if not ret or frame.empty():
                 raise Exception("Camera opens but does not manage to read frame")
 
@@ -63,6 +62,7 @@ class AutoVideoCapture(object):
 
         self.idx = idx
         self.name = str(idx)
+        self.fps = fps
         self.frame_count = 0
         self.is_started = False
         self.video_writer = None
@@ -73,10 +73,11 @@ class AutoVideoCapture(object):
             return
 
         self.is_started = True
-        # todo set name?
+        
         out_dirpath = self.out_dir +"/"+ self.name
         if not os.path.exists(out_dirpath):
             os.makedirs(out_dirpath)
+            Logger.info("Start recording in " + out_dirpath)
         else:
             raise Exception("{0} Exists already".format(out_dirpath))
 
@@ -85,8 +86,9 @@ class AutoVideoCapture(object):
 
     def read(self):
         ret,frame = self.stream.read()
+        if frame is None:
+			return None
         self.frame_size = frame.shape[:2]
-        print self.frame_size
         if ret:
             cv2.imshow(self.name, frame)
             return frame
@@ -97,22 +99,24 @@ class AutoVideoCapture(object):
     def _write_to_file(self, frame):
 
         if self.frame_count % VIDEO_CHUNK_SIZE == 0:
-
+			
             self.frame_count / VIDEO_CHUNK_SIZE
-            out_filename = self.out_dir +"/"+ self.name + "/{0}_{1}.avi".format(self.name,
-                                                                     self.frame_count / VIDEO_CHUNK_SIZE)
+            out_filename = self.out_dir +"/"+ self.name + "/{0}_{1}.{2}".format(self.name,
+                                                                     self.frame_count / VIDEO_CHUNK_SIZE, VIDEO_FORMAT["extension"])
+                                                                     
+            Logger.info("Device \"{0}\" Making new video chunk:{1}".format(self.name,out_filename))
+			
             try:
                 self.video_writer.close()
             except:
                 pass
 
-            self.video_writer = cv2.VideoWriter(out_filename, VIDEO_FORMAT, FPS, self.frame_size, True)
-            print out_filename, VIDEO_FORMAT, FPS, self.frame_size
+            self.video_writer = cv2.VideoWriter(out_filename, VIDEO_FORMAT["fourcc"], self.fps , self.frame_size, True)
+            
 
         assert(self.video_writer.isOpened())
 
         self.video_writer.write(frame)
-        print frame.shape, self.video_writer
 
     def capture(self):
         frame = self.read()
@@ -121,7 +125,6 @@ class AutoVideoCapture(object):
             return
 
         if frame is not None:
-
             self._write_to_file(frame)
             self.frame_count += 1
 
