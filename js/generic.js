f122f7a25a92327fa25bfde752d8b2c38425ae7f
Quentin Geissmann <qgeissmann@gmail.com>
2014-06-03 13:43:51 +0100

    website /DAG squeleton

--- js/generic.js
+++ js/generic.js
@@ -1,3 +1,5 @@
+prefixes = ["dag", "fig", "math"]
+
 $( document ).ready(function() {
 	$("ul.navbar-nav li").removeClass('active');
 	var page = $(location).attr('href');
@@ -6,3 +8,17 @@ $( document ).ready(function() {
 	console.log($('ul.navbar-nav li a[href$="'+page+'"]'));
 	$('ul.navbar-nav li a[href$="'+page+'"]').parents().addClass('active');
 	});
+
+var updateDag = function(tag){
+	
+	suffix = tag.split("_")[1]
+	console.log(prefixes)
+	console.log(suffix)
+	for(p in prefixes){
+		$('.' + prefixes[p] + '_div .process').attr("class", "process");
+		id = '#' + prefixes[p] + '_' + suffix
+		console.log(id)
+		$(id).attr("class", "process active");
+		}
+	};
+
