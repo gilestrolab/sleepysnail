902201e4efebb39f75fa25f7a2fd61382f248eb6
Quentin Geissmann <qgeissmann@gmail.com>
2014-06-07 16:28:13 +0100

    dealing with historygit add .

--- js/generic.js
+++ js/generic.js
@@ -8,29 +8,22 @@ var media_map = {"raw":"1044213",
 "hist": "1045574",
 "contours": "1041210"};
 
+
 var media_url_template = ["http://wl.figshare.com/articles/", 
 						  "/embed?show_title=0"];
 
 
-$( document ).ready(function() {
-	$("ul.navbar-nav li").removeClass('active');
-	var page = $(location).attr('href');
-	
-	$('ul.navbar-nav li a[href$="'+page+'"]').parents().addClass('active');
-	//resetDag("")
-	});
-
-var resetDag = function(tag){
-	for(p in prefixes){
-		$('.' + prefixes[p] + '_div .process').attr("class", "process active");
-		}
+var urlForProcess = function(){
+	hash = location.hash;
+	console.log(location.hash)
+	suffix = hash.split("#")[1]
+	if(hash){
+		showAllSuff(suffix)
+	}
 	};
 
 
-
-var updateDag = function(tag){
-	
-	suffix = tag.split("_")[1]
+var showAllSuff = function(suffix){
 	for(p in prefixes){
 		$('.' + prefixes[p] + '_div .process').attr("class", "process");
 		id = '#' + prefixes[p] + '_' + suffix
@@ -38,10 +31,31 @@ var updateDag = function(tag){
 		//$(id).html($(id).html());
 		if(prefixes[p] == "fig"){
 			url = media_url_template[0]+ media_map[suffix]+media_url_template[1];
-			$(".process iframe").attr("src", "");
-			$(id).find("iframe").attr("src", url);
+			var container = $(id).find("div.iframe_container");
+			var newFrame = document.createElement("iframe");
+			newFrame.src =  url;
+			
+			newFrame.width=568;
+			newFrame.height= 481;
+			
+			container.html(newFrame);
 			}
 		}
-	
+	}
+var updateDag = function(tag){
+	suffix = tag.split("_")[1];
+	location.hash = '#'+ suffix;
 	};
 
+$( document ).ready(function() {
+	$("ul.navbar-nav li").removeClass('active');
+	var page = $(location).attr('href');
+	
+	$('ul.navbar-nav li a[href$="'+page+'"]').parents().addClass('active');
+	console.log("ERADYYYYYYYYYYYY")
+	urlForProcess()
+	
+	});
+
+$(window).on('hashchange', urlForProcess);
+	
